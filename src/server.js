d9c5d9ff1b5c5363f022c9d26c5659f3a0557c76
Rafi <ammar18rafi@gmail.com>
2024-05-08 11:25:54 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const routes = require('./routes');
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
